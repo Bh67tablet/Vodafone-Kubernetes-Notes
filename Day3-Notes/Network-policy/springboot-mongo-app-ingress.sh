403608acd97724ec69486517a3ec04f4652110b8
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-27 11:35:08 +0100

    Delete Day3-Notes/Network-policy/springboot-mongo-app-ingress.sh

--- Day3-Notes/Network-policy/springboot-mongo-app-ingress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-kubectl apply -f springboot-mongo-app.yml
-kubectl get services springboot-app-svc
-kubectl apply -f deny-ingress-to-mongodb-and-springapp.yaml
-kubectl get netpol
-kubectl get services springboot-app-svc
-kubectl apply -f allow-ingress-to-springapp-from-all.yaml
-kubectl get services springboot-app-svc
-kubectl apply -f allow-ingress-to-mongodb-from-springapp.yaml
