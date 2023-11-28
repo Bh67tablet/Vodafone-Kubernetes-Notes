ace337fe8e50e6847de8b97f5e3174f352ca3103
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-28 14:15:10 +0100

    Update nfsConfig.sh

--- Course-End-Project-Steps/nfsConfig.sh
+++ Course-End-Project-Steps/nfsConfig.sh
@@ -3,7 +3,7 @@ apt -y update
 apt -y install nfs-kernel-server
 chown nobody:nogroup /data/
 chmod 777 /data/
-echo "/data *(rw,sync,no_root_squash)" >>  /etc/exports
+echo "/data *(rw,sync,no_root_squash)" >> /etc/exports
 exportfs -rv
 systemctl restart nfs-kernel-server
 apt -y update
